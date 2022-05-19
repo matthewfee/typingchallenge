1291950f30dfa080e6040a0bae1c7b42b7407e3a
Matt <60991158+matthewfee@users.noreply.github.com>
2022-05-19 16:11:37 -0600

    changed theme highlight color to purple

--- src/styled/Themes.tsx
+++ src/styled/Themes.tsx
@@ -1,11 +1,11 @@
 export const darkTheme = {
   mainBgColor: '#333',
   mainTextColor: '#f9f9f9',
-  accentColor: '#e16365'
+  accentColor: '#ab7eff'
 }
 
 export const lightTheme = {
   mainBgColor: '#f9f9f9',
   mainTextColor: '#333',
-  accentColor: '#e16365'
+  accentColor: '#7830FA'
 }
